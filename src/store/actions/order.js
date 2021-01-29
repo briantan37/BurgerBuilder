2bd130ab94b7a4edfee27c2cc022cedaa281a761
Brian Tan <briantan37@gmail.com>
2021-01-29 10:16:13 -0800

    Refactored checkout container render method

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -25,7 +25,7 @@ export const purchaseBurgerStart = () => {
 export const purchaseBurger = (orderData) => {
     return dispatch => {
         dispatch(purchaseBurgerStart());
-        axios.post('/orders.json', order)
+        axios.post('/orders.json', orderData)
             .then(response => {
                 dispatch(purchaseBurgerSuccess(response.data, orderData));
             })
