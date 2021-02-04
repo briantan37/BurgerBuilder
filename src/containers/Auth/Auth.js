238fd6882c78fdcca9185581ec687f60401f6d79
Brian Tan <briantan37@gmail.com>
2021-02-04 13:54:33 -0800

    Fixed styling and form not displaying

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import classes from './Auth.css';
 
 class Auth extends Component {
     state = {
@@ -39,10 +40,10 @@ class Auth extends Component {
 
     render() {
         const formElementsArray = [];
-        for (let key in this.state.orderForm) {
+        for (let key in this.state.controls) {
             formElementsArray.push({
                 id: key,
-                config: this.state.orderForm[key]
+                config: this.state.controls[key]
             })
         }
         const form = formElementsArray.map(formElement => (
@@ -58,7 +59,7 @@ class Auth extends Component {
             />
         ))
         return (
-            <div>
+            <div className={classes.Auth}>
                 <form>
                     {form}
                     <Button btnType="Success">Submit</Button>
