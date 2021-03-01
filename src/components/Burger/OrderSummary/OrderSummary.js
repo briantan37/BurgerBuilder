23110822f50b0b507ef7f307f34f9191ba920e98
Brian Tan <briantan37@gmail.com>
2021-03-01 14:59:44 -0800

    removed more console logs

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,19 +4,15 @@ import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
-    componentWillUpdate() {
-        console.log("[OrderSummary] WillUpdate");
-    }
-
     render() {
         const ingredientSummary = Object.keys(this.props.ingredients)
-        .map(igKey => {
-            return (
-                <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
-                </li>
-            );
-        })
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+                    </li>
+                );
+            })
         return (
             <Aux>
                 <h3>Your Order</h3>
