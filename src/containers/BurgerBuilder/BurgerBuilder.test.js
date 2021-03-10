8e7c733b889f7fe64cb53d424c58b816ef461550
Brian Tan <briantan37@gmail.com>
2021-03-09 23:12:08 -0800

    Fixed testing bug in BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.test.js
+++ src/containers/BurgerBuilder/BurgerBuilder.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BurgerBuilder } from './BurgerBuilder';
 
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import { BurgerBuilder } from './BurgerBuilder';
 
 configure({ adapter: new Adapter() });
 
